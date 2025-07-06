ea0b112099deee559b4a313c972411c944b810cd
doruche <doruche18@outlook.com>
2025-07-06 15:53:01 +0800

    try

---
+++ ch7/regfile.sv
@@ -0,0 +1,17 @@
+module regfile (
+    input logic clk,
+    input logic we3,
+    input logic [4:0] ra1, ra2, wa3,
+    input logic [31:0] wd3,
+    output logic [31:0] rd1, rd2
+);
+    logic [31:0] rf [31:0];
+
+    always_ff @( posedge clk ) begin
+        if (we3) rf[wa3] <= wd3;
+    end
+
+    assign rd1 = (ra1 == 0) ? 32'b0 : rf[ra1];
+    assign rd2 = (ra2 == 0) ? 32'b0 : rf[ra2];
+
+endmodule
